1c188ccf41464519aa26899fa512f036b8711b5a
mario ruby <mariogabrielrn@gmail.com>
2023-02-15 17:48:49 +0100

    doneee

--- models/User.model.js
+++ models/User.model.js
@@ -2,11 +2,26 @@ const { Schema, model } = require("mongoose");
 
 // TODO: Please make sure you edit the user model to whatever makes sense in this case
 const userSchema = new Schema({
-  username: {
+      email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim:true,
+    },
+    username: {
     type: String,
-    unique: true
+    unique: true,
+    required: false,
+    trim:true
   },
-  password: String
+  password: {
+    type: String,
+    required: false
+  }
+},
+  {
+  timestamps: true
 });
 
 const User = model("User", userSchema);
